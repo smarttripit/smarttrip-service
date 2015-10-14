07e7942a57bad714f846f0ebd3a097dc3fa93913
gaoweibupt <woshi73>
2015-10-14 20:22:51 +0800

    rename RouteTheme

---
+++ src/main/java/com/smarttrip/dao/RouteThemeMapper.java
@@ -0,0 +1,16 @@
+package com.smarttrip.dao;
+
+import com.smarttrip.domain.RouteTheme;
+import java.util.List;
+
+public interface RouteThemeMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(RouteTheme record);
+
+    RouteTheme selectByPrimaryKey(String id);
+
+    List<RouteTheme> selectAll();
+
+    int updateByPrimaryKey(RouteTheme record);
+}
